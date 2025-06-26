9629015437df3a09d9627da6c7145e369199a8f0
wladev <wladimir.perfiloff.dev@gmail.com>
2025-06-26 18:32:14 +0200

    test prod docker ok

---
+++ init.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Corriger les permissions (récupère le bon utilisateur dans le container)
+echo "Fixing permissions on var/"
+chown -R www-data:www-data var
+# Générer les assets (avec Symfony AssetMapper + Tailwind)
+php bin/console asset-map:compile
+php bin/console importmap:install
+php bin/console tailwind:build
+
+
+# Installer les dépendances PHP
+composer install --no-dev --optimize-autoloader
+
+# Clear + warmup cache
+php bin/console cache:clear --no-warmup
+php bin/console cache:warmup
+
+echo "Fixing ownerships"
+chown -R www-data:www-data var vendor public
+
+echo "Fixing permissions"
+find var -type d -exec chmod 775 {} \;
+find var -type f -exec chmod 664 {} \;
+chown -R www-data:www-data /var/www
+
+# Démarrer php-fpm
+php-fpm

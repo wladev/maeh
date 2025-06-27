750c0133a8b35c00228e563385bbe52aa3232e7a
wladev <wladimir.perfiloff.dev@gmail.com>
2025-06-27 12:20:45 +0200

    green

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@
 echo "Fixing permissions on var/"
 chown -R www-data:www-data var
 # Générer les assets (avec Symfony AssetMapper + Tailwind)
+chown www-data:www-data /var/tailwind/v3.4.17/tailwindcss-linux-x64
+chmod +x /var/tailwind/v3.4.17/tailwindcss-linux-x64
 php bin/console asset-map:compile
 php bin/console importmap:install
 php bin/console tailwind:build
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     './public/index.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'marquee-left-to-right': 'leftToRight 8s linear infinite',
+      },
+      keyframes: {
+        leftToRight: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(350%)' },
+        },
+      }
+    },
   },
   plugins: [],
 }

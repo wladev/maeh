7d95ecdb5ac2ded785820f2af30082c7e175779a
wladev <wladimir.perfiloff.dev@gmail.com>
2025-07-01 09:29:19 +0200

    header_bg

--- init.sh
+++ init.sh
@@ -4,8 +4,8 @@
 echo "Fixing permissions on var/"
 chown -R www-data:www-data var
 # Générer les assets (avec Symfony AssetMapper + Tailwind)
-chown www-data:www-data /var/tailwind/v3.4.17/tailwindcss-linux-x64
-chmod +x /var/tailwind/v3.4.17/tailwindcss-linux-x64
+#chown www-data:www-data /var/tailwind/v3.4.17/tailwindcss-linux-x64
+#chmod +x /var/tailwind/v3.4.17/tailwindcss-linux-x64
 php bin/console asset-map:compile
 php bin/console importmap:install
 php bin/console tailwind:build
@@ -25,6 +25,6 @@ echo "Fixing permissions"
 find var -type d -exec chmod 775 {} \;
 find var -type f -exec chmod 664 {} \;
 chown -R www-data:www-data /var/www
-
+chmod +x var/tailwind/v3.4.17/tailwindcss-linux-x64
 # Démarrer php-fpm
 php-fpm
